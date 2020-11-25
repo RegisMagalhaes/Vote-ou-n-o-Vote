7cd69d3d030e7a417d4cea88aed1f53279ceeeff
Regis Leandro da Silva Magalhães <engenheiro.regis.magalhaes@gmail.com>
2020-11-25 00:59:33 -0300

    Este programa decide se uma pessoa deve votar com base na sua idade

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace condicionais_EX1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o ano atual: ");
+            int hoje = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o ano em que você nasceu: ");
+            int nasc = int.Parse(Console.ReadLine());
+
+            int vote = hoje - nasc;
+
+            if(vote >= 18){
+                Console.WriteLine("Cumpra seu dever de cidadão e vote!");
+            }else {
+                Console.WriteLine("Você ainda não deve votar! Aguarde completar a idade!");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
